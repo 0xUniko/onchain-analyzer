7aa1ca35e1e05ddd7a182b11a0bc864fbaf4327c
name <email>
2022-01-15 23:38:54 +0800

    feat: change scan to client

--- utils/bsc_client.py
+++ utils/bsc_client.py
@@ -1,9 +1,8 @@
-# %%
 import httpx
 from dotenv import dotenv_values
 from web3 import Web3, HTTPProvider
 from web3.middleware import geth_poa_middleware
-from tenacity import Retrying, retry, wait_random, stop_after_attempt
+from tenacity import retry, wait_random, stop_after_attempt
 
 api_key = dotenv_values()['BSCSCAN_API_KEY']
 url = 'https://api.bscscan.com/api'
@@ -13,10 +12,26 @@ w3 = Web3(HTTPProvider(endpoint))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
-@retry(stop=stop_after_attempt(1),
-       wait=wait_random(min=1, max=1.5),
-       reraise=True)
-def scan(module: str, action: str, **kwargs):
-    params = {'module': module, 'action': action, 'apiKey': api_key, **kwargs}
+class Scanner():
+    def __init__(self):
+        self.client = httpx.Client()
 
-    return httpx.get(url=url, params=params).json()['result']
+    @retry(stop=stop_after_attempt(1),
+           wait=wait_random(min=1, max=1.5),
+           reraise=True)
+    def scan(self, module: str, action: str, **kwargs):
+        params = {
+            'module': module,
+            'action': action,
+            'apiKey': api_key,
+            **kwargs
+        }
+
+        return self.client.get(url=url, params=params).json()['result']
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, trace):
+        self.client.close()
+        return False
