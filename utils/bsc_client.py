e85a5139b22ecc11e266fac0faf0832f42855b7a
name <email>
2022-01-18 11:14:31 +0800

    refactor: add api and key to the object state

--- utils/bsc_client.py
+++ utils/bsc_client.py
@@ -5,7 +5,8 @@ from web3.middleware import geth_poa_middleware
 from tenacity import retry, wait_random, stop_after_attempt
 
 api_key = dotenv_values()['BSCSCAN_API_KEY']
-url = 'https://api.bscscan.com/api'
+api = 'https://api.bscscan.com/api'
+# api = 'https://api-test.bscscan.com/api'
 endpoint = dotenv_values()['ANKR_BSC_ENDPOINT']
 
 w3 = Web3(HTTPProvider(endpoint))
@@ -13,6 +14,10 @@ w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
 class Scanner():
+    def __init__(self, api=api, key=api_key):
+        self.api = api
+        self.key = key
+
     @retry(stop=stop_after_attempt(1),
            wait=wait_random(min=1, max=1.5),
            reraise=True)
@@ -20,11 +25,11 @@ class Scanner():
         params = {
             'module': module,
             'action': action,
-            'apiKey': api_key,
+            'apiKey': self.key,
             **kwargs
         }
 
-        return self.client.get(url=url, params=params).json()['result']
+        return self.client.get(url=self.api, params=params).json()['result']
 
     @retry(stop=stop_after_attempt(1),
            wait=wait_random(min=1, max=1.5),
@@ -33,20 +38,20 @@ class Scanner():
         params = {
             'module': module,
             'action': action,
-            'apiKey': api_key,
+            'apiKey': self.key,
             **kwargs
         }
 
-        res = await self.client.get(url=url, params=params)
+        res = await self.client.get(url=self.api, params=params)
 
         return res.json()['result']
 
     def __enter__(self):
-        self.client = httpx.Client()
+        self.client = httpx.Client(proxies='http://127.0.0.1:10809')
         return self
 
     async def __aenter__(self):
-        self.client = httpx.AsyncClient()
+        self.client = httpx.AsyncClient(proxies='http://127.0.0.1:10809')
         return self
 
     def __exit__(self, exc_type, exc_value, trace):
