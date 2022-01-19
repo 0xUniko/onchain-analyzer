1bfe765265a2ecb0b375c261d1e30be9a06f7d8b
name <946153376@qq.com>
2022-01-20 00:18:00 +0800

    feat: add more constants

--- utils/pancake_utils.py
+++ utils/pancake_utils.py
@@ -4,7 +4,11 @@ pancake_factory_address = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
 pairCreated_topic = w3.keccak(
     text='PairCreated(address,address,address,uint256)').hex()
 wbnb_addr = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
+busd_addr = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
 wbnb_topic = '0x000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+busd_topic = '0x000000000000000000000000e9e7cea3dedca5984780bafc599bd69add087d56'
+bscusd_topic = '0x00000000000000000000000055d398326f99059ff775485246999027b3197955'
+doge_topic = '0x000000000000000000000000ba2ae424d960c26247dd6c32edc70b295c744c43'
 
 pancake_router_address = '0x10ed43c718714eb63d5aa57b78b54704e256024e'
 pancake_router_topic = '0x00000000000000000000000010ed43c718714eb63d5aa57b78b54704e256024e'
