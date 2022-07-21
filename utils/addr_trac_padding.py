6cb0f783ef723ec0ef3c977e4fdbcbe3ed562f2b
ls <946153376@qq.com>
2022-07-21 15:40:21 +0800

    feat: add typing

--- utils/addr_trac_padding.py
+++ utils/addr_trac_padding.py
@@ -1,6 +1,22 @@
-def addr_trac(x: str):
-    return '0x' + x[26:66]
+from eth_typing.evm import HexAddress
+from eth_typing.encoding import HexStr
+from hexbytes import HexBytes
+from typing import cast
 
 
-def addr_padding(x: str):
-    return '0x' + '0' * 24 + x[2:]
+def addr_trac(x: HexStr | HexBytes) -> HexAddress:
+    if isinstance(x, HexBytes):
+        h = x.hex()
+    else:
+        h = x
+
+    return cast(HexAddress, '0x' + h[26:66])
+
+
+def addr_padding(x: HexStr | HexBytes) -> HexAddress:
+    if isinstance(x, HexBytes):
+        h = x.hex()
+    else:
+        h = x
+
+    return cast(HexAddress, '0x' + '0' * 24 + h[2:])
