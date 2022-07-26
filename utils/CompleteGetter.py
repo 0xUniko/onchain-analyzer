c8da0b40866a2d68fcb54ec606999323a73482e3
ls <946153376@qq.com>
2022-07-26 14:01:27 +0800

    feat: add print(f'get all txs from {address}')

--- utils/CompleteGetter.py
+++ utils/CompleteGetter.py
@@ -20,6 +20,7 @@ class CompleteGetter():
 
     def get_all(self, address: HexAddress | Address,
                 action: Literal['txlist'] | Literal['tokennfttx']):
+        print(f'get all txs from {address}')
         new_txs_collector = []
         startblock = 1
 
