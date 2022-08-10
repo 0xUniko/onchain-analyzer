e54f73c54d6ab797dd5e03a209ea40ce4afdd596
ls <946153376@qq.com>
2022-08-10 16:29:54 +0800

    feat: add get_token_name_cached

--- utils/get_token_name.py
+++ utils/get_token_name.py
@@ -10,3 +10,23 @@ def get_token_name(address: ChecksumAddress) -> str:
     contract = w3.eth.contract(address, abi=general_abi)
 
     return contract.caller.name()
+
+
+def get_token_name_cached(address: ChecksumAddress) -> str:
+    with open('utils/token_names.json', 'r') as f:
+        token_names = json.load(f)
+
+    if address in token_names:
+        return token_names[address]
+    else:
+        with open('utils/general_abi.json', 'r') as f:
+            general_abi = json.load(f)
+        contract = w3.eth.contract(address, abi=general_abi)
+
+        name = contract.caller.name()
+        token_names[address] = name
+
+        with open('utils/token_names.json', 'w') as f:
+            json.dump(token_names, f)
+
+        return name
